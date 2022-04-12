4c584e09dd6c2545c5532e5a3220d644890793cb
dimitrijepanic <mitadimitrije.panic@gmail.com>
2022-04-12 16:38:53 +0200

    Added Player Cards and Field Coloring

--- app/src/main/java/com/example/monopoly/MainActivity.java
+++ app/src/main/java/com/example/monopoly/MainActivity.java
@@ -2,13 +2,53 @@ package com.example.monopoly;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 
+import com.example.monopoly.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private Canvas canvas;
+
+    private static int [] playerColor = {Color.RED, Color.BLUE, Color.YELLOW, Color.GREEN};
+    private static int fieldSize = 32;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setPlayers(4);
+
+    }
+
+    private int throwDice(){
+        return 0;
+    }
+
+    private void setPlayers(int numPlayers){
+        Bitmap bitmap = Bitmap.createBitmap(400, 400, Bitmap.Config.ARGB_8888);
+        bitmap = bitmap.copy(bitmap.getConfig(), true);
+        canvas = new Canvas(bitmap);
+        Paint paint = new Paint();
+
+        paint.setStyle(Paint.Style.FILL_AND_STROKE);
+        paint.setAntiAlias(true);
+
+        for(int i = 0; i < numPlayers; i++){
+            paint.setColor(playerColor[i]);
+            canvas.drawCircle(370, 370, 10, paint);
+            //canvas.drawRect(312,396,344,400, paint);
+        }
+
+        binding.boardImg.setImageBitmap(bitmap);
+        binding.boardImg.setBackgroundResource(R.drawable.board);
     }
 }
